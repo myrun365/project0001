ac3aff64d4c0e156dfb387b55ce913bfba10fc3e
myrun365 <283374323@qq.com>
2020-04-23 23:42:46 +0800

    Initial commit

---
+++ JavaWeb01/web/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: mayang
+  Date: 2020/4/23
+  Time: 12:12
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Mayang_Web</title>
+  </head>
+  <body>
+  <h1 style="color: orangered">Hello mayang's Web!!! </h1>
+  <h1 style="color: orangered">Hello mayang's Web!!! </h1>
+  </body>
+</html>
---
+++ module01/src/com/mayang/java/customer.java
@@ -0,0 +1,41 @@
+package com.mayang.java;
+
+public class customer {
+
+    public static void main(String[] args){
+        int num1 = 10;
+        int num2 = 20;
+//sout  /soutp  /soutm   /soutv   / xxx.sout
+        System.out.println("HelloWorld!!!");
+        System.out.println("customer.main");
+        System.out.println("args = [" + args + "]");
+        System.out.println("customer.main");
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+
+//fori
+        String[] arr = new String[]{"mayang","Tom","Teson"};
+        for (int i = 0; i <arr.length ; i++) {
+            System.out.println(arr[i]);
+        }
+//iter
+        for (String s : arr) {
+            System.out.println(s);
+            
+        }
+
+//itar
+        for (int i = 0; i < arr.length; i++) {
+            String s = arr[i];
+            System.out.println(s);
+        }
+
+
+
+
+    }
+
+
+
+}
